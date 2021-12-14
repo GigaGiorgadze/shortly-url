3063f1709111306548164b22a6dae8d57fad9a53
Giga <giga.giorgadze.11@gmail.com>
2021-12-14 14:31:57 +0400

    inital commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// button.addEventListener('click', () => {
+//     let url = 'https://api.shrtco.de/v2/shorten?url=https://www.w3schools.com/js/js_api_fetch.asp';
+//     let req = new XMLHttpRequest()
+
+//     req.open('GET', url, true)
+//     req.onload = () => {
+//         let result = JSON.parse(req.responseText)
+//         console.log(result.result)
+//     }
+//     req.send()
+// })
